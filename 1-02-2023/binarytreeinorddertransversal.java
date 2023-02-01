a8a8091ebb8ac0e740f4b7803d1219327f1a6221
Bala-sravani-08 <123641194+Bala-sravani-08@users.noreply.github.com>
2023-02-01 21:38:18 +0530

    Create binarytreeinorddertransversal.java

---
+++ 1-02-2023/binarytreeinorddertransversal.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void InOrder(TreeNode root, List list){
+        if(root== null){
+            return ;
+        }
+        InOrder(root.left,list);
+        list.add(root.val);
+        InOrder(root.right,list);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        InOrder(root,list);
+        return list;
+    }
+}
